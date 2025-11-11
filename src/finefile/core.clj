fa24773632ae627f4fe7a3d02870998c77ba4845
John Shaffer <jdsha@pm.me>
2025-11-10 23:49:01 -0600

    Fix cleanup being specified more than once
    --cleanup is only allowed to be specified once.
    Move it from being specified for each command
    to just being specified once per run.

--- src/finefile/core.clj
+++ src/finefile/core.clj
@@ -1,20 +1,20 @@
 (ns finefile.core)
 
 (defn command->hyperfine-args [k command]
-  (let [{:strs [cleanup conclude command prepare]} command]
+  (let [{:strs [conclude command prepare]} command]
     (concat
       ["--command-name" k]
       (when prepare ["--prepare" prepare])
       (when command [command])
-      (when conclude ["--conclude" conclude])
-      (when cleanup ["--cleanup" cleanup]))))
+      (when conclude ["--conclude" conclude]))))
 
 (defn finefile-map->hyperfine-args [finefile-map]
   (let [{:strs [commands defaults]} finefile-map
-        {:strs [export-json shell]} defaults]
+        {:strs [cleanup export-json shell]} defaults]
     (concat
       (when export-json ["--export-json" export-json])
       (when shell ["--shell" shell])
+      (when cleanup ["--cleanup" cleanup])
       (mapcat
         (fn [[k command]]
           (command->hyperfine-args k (merge defaults command)))
