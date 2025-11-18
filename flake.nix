238d70f0e2e9e8b17eb9636da112918e700f6dc9
John Shaffer <jdsha@pm.me>
2025-11-18 08:59:10 -0600

    Update deps
    clj-nix needs older nixpkgs currently due to
    a graalvm package rename, but
    hyperfine-flake needs latest nixpkgs-unstable for
    rustc. Address by letting hyperfine-flake use its
    own nixpkgs.

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
     flake-utils.url = "github:numtide/flake-utils";
     hyperfine-flake = {
       inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:john-shaffer/hyperfine-flake";
     };
   };
