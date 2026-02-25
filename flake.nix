0d8bd472d5999ae9a46862d12263fb05946ab7b2
John Shaffer <jdsha@pm.me>
2026-02-25 14:01:14 -0600

    Remove darwin from supported systems

--- flake.nix
+++ flake.nix
@@ -21,9 +21,7 @@
     with inputs;
     let
       supportedSystems = [
-        "aarch64-darwin"
         "aarch64-linux"
-        "x86_64-darwin"
         "x86_64-linux"
       ];
       forAllSystems =
